9eebe4191532281ceceb643ac8117fe6dc66edb4
Michael-Rozenfeld <michaelroz85@gmail.com>
2022-05-06 12:13:23 +0300

    package.json now start the front and back together

---
+++ server/constants/SelectData.js
@@ -0,0 +1,66 @@
+export const gendrerOptions = [
+  { option: "זכר", value: 1 },
+  { option: "נקבה", value: 2 },
+  { option: "אחר", value: 3 },
+];
+
+export const familyStatusOptions = [
+  { option: "רווק/ה", value: 1 },
+  { option: "נשואי/ה", value: 2 },
+  { option: "גרוש/ה", value: 3 },
+  { option: "אלמן/ה", value: 4 },
+  { option: "אחר", value: 5 },
+];
+
+export const hospitalOptions = [
+  { option: "יוספטל", value: 1 },
+  { option: "ברזילי", value: 2 },
+  { option: "אסף הרופא", value: 3 },
+  { option: "סורוקה", value: 4 },
+  { option: "אסותא באר שבע", value: 5 },
+  { option: "מעייני הישועה", value: 6 },
+  { option: "מ.ר.ב מרכז רפואי", value: 7 },
+  { option: "מרכז רפואי הרצליה", value: 8 },
+  { option: "הלל יפה", value: 9 },
+  { option: "וולפסון", value: 10 },
+  { option: "רמב'ם", value: 11 },
+  { option: "הכרמל", value: 12 },
+  { option: "רמב''ם", value: 13 },
+  { option: "בני ציון", value: 14 },
+  { option: "כרמל", value: 15 },
+  { option: "האיטלקי", value: 16 },
+  { option: "אלישע", value: 17 },
+  { option: "חורב", value: 18 },
+  { option: "אסותא חיפה", value: 19 },
+  { option: "הדסה עין כרם", value: 20 },
+  { option: "הדסה הר הצופים", value: 21 },
+  { option: "שערי צדק", value: 22 },
+  { option: "אל מקאסד", value: 23 },
+  { option: "אוגוסטה ויקטוריה", value: 24 },
+  { option: "מאיר", value: 25 },
+  { option: "נהריה", value: 26 },
+  { option: "לניאדו", value: 27 },
+  { option: "רבין ק.בלינסון", value: 28 },
+  { option: "זיו צפת", value: 29 },
+  { option: "אסותא ראשון לציון", value: 30 },
+  { option: "קפלן", value: 31 },
+  { option: "שיבא", value: 32 },
+  { option: "אסותא רמת החייל", value: 33 },
+  { option: "סוראסקי", value: 34 },
+  { option: "אחר", value: 35 },
+];
+export const insuranseOptions = [
+  { option: "מכבי", value: 1 },
+  { option: "כללית", value: 2 },
+  { option: "מאוחדת", value: 3 },
+  { option: "לאומית", value: 4 },
+  { option: "אחר", value: 5 },
+];
+export const languageOptions = [
+  { option: "עברית", value: 1 },
+  { option: "ערבית", value: 2 },
+  { option: "אנגלית", value: 3 },
+  { option: "רוסית", value: 4 },
+  { option: "אמהרית", value: 5 },
+  { option: "אחר", value: 6 },
+];
