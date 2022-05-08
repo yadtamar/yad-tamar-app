872b38a73693e8c59f9074c5a8d7a8bcf8ca4b1c
kirilreznik <kirilreznik@gmail.com>
2022-05-08 08:10:53 +0300

    removed default data

--- client/src/pages/create-family-page/CreateFamilyPage.js
+++ client/src/pages/create-family-page/CreateFamilyPage.js
@@ -11,42 +11,24 @@ const CreateFamilyPage = () => {
   const [step, setStep] = useState(1);
   const [image, setImage] = useState(null);
   const [data, setData] = useState({
-    first_name: "test",
-    last_name: "test",
-    home_phone: "123456789",
-    cell_phone: "1234567890",
-    mail: "test@test.com",
-    address: "test",
-    city: "test",
-    age: "1",
-    gender: "1",
-    family_status: "1",
-    kids_num: "1",
-    language: "1",
-    sickness: "test",
-    hospital: "1",
-    medical_insurance: "1",
-    medical_history: "test",
+    first_name: "",
+    last_name: "",
+    home_phone: "",
+    cell_phone: "",
+    mail: "",
+    address: "",
+    city: "",
+    age: "",
+    gender: "",
+    family_status: "",
+    kids_num: "",
+    language: "",
+    sickness: "",
+    hospital: "",
+    medical_insurance: "",
+    medical_history: "",
   });
-  /* useEffect(() => {
-    if (id) {
-      fetch(
-        `http://ec2-18-195-126-1.eu-central-1.compute.amazonaws.com:5000/families/${id}`
-      )
-        .then((response) => {
-          if (response.ok) {
-            return response.json();
-          }
-          throw response;
-        })
-        .then((data) => setData(...data))
-        .catch((err) => {
-          if (err.statusText !== "OK") {
-            console.log(err);
-          }
-        });
-    }
-  }, []); */
+
   return (
     <PageLayout
       pageComponent={
