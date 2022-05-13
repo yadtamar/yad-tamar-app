a6e15a644ad49d4e0dcb794be0b065997ed95c28
daniel961 <danielglx961@gmail.com>
2022-05-13 16:30:12 +0300

    refactore

--- server/db.js
+++ server/db.js
@@ -2,10 +2,8 @@ const Pool = require("pg").Pool;
 require("dotenv").config();
 const fs = require("fs");
 
-
-
 const password = process.env.Password;
-console.log(password);
+
 const pool = new Pool({
   user: "doadmin",
   password,
