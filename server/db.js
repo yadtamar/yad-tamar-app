33679dc41714a48737e1ea0b32c40108dbb4efae
daniel961 <danielglx961@gmail.com>
2022-05-13 16:20:35 +0300

    add dirname

--- server/db.js
+++ server/db.js
@@ -1,5 +1,9 @@
 const Pool = require("pg").Pool;
 require("dotenv").config();
+const path = require('path')
+
+console.log(__dirname);
+
 const fs = require("fs");
 const password = process.env.Password;
 const pool = new Pool({
@@ -11,7 +15,7 @@ const pool = new Pool({
   ssl: {
     rejectUnauthorized: false,
     ca: fs.readFileSync('ca-certificate.crt').toString(),
-   },
+  },
 });
 
 module.exports = pool;
