8ba33002ca87c2fc2c2b8736380361a2eafd34d5
kirilreznik <kirilreznik@gmail.com>
2022-05-17 20:54:37 +0300

    set up tasks logic

--- client/src/pages/tasks-page/TasksPage.jsx
+++ client/src/pages/tasks-page/TasksPage.jsx
@@ -1,14 +1,73 @@
-import TaskElement from "./components/TaskElement";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { Typography } from "@mui/material";
+import TaskElement from "./components/TaskElement";
 import "./TasksPage.css";
-
+import logo from "../../assets/yad-tamar-logo.png";
 function TasksPage() {
-  const [tasks, setTasks] = useState([]);
+  const volunteerId = 1;
   const { family_id } = useParams();
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const [filterBy, setFilterBy] = useState("all");
+  const [tasks, setTasks] = useState([
+    {
+      task_name: "לקחת את הדואר",
+      task_id: 58,
+      family_id: 160,
+      helper_id: 2,
+      date: "2022-05-16T00:00:00.000Z",
+      comments: "",
+      createdAt: null,
+      updatedAt: null,
+    },
+    {
+      task_name: "task1",
+      task_id: 59,
+      family_id: 160,
+      helper_id: 5,
+      date: "2022-05-16T00:00:00.000Z",
+      comments: "task1",
+      createdAt: true,
+      updatedAt: null,
+    },
+    {
+      task_name: "task2",
+      task_id: 60,
+      family_id: 160,
+      helper_id: null,
+      date: "2022-05-16T00:00:00.000Z",
+      comments: "task2",
+      createdAt: null,
+      updatedAt: null,
+    },
+    {
+      task_name: "לקנות מזון",
+      task_id: 61,
+      family_id: 160,
+      helper_id: null,
+      date: "2022-05-11T00:00:00.000Z",
+      comments: "",
+      createdAt: null,
+      updatedAt: null,
+    },
+  ]);
+  const [filteredTasks, setFilteredTasks] = useState(tasks);
 
   useEffect(() => {
+    if (tasks) {
+      switch (filterBy) {
+        case "all":
+          return setFilteredTasks(tasks);
+        case "currentUser":
+          const currentUserTasks = tasks.filter((task) => {
+            return task.helper_id === volunteerId;
+          });
+          setFilteredTasks(currentUserTasks);
+      }
+    }
+  }, [filterBy]);
+
+  /* useEffect(() => {
     const fetchTasks = async () => {
       try {
         const response = await fetch(
@@ -22,16 +81,62 @@ function TasksPage() {
       } catch (error) {}
     };
     fetchTasks();
-  }, []);
+  }, []); */
 
   return loading ? (
     <span>Loading...</span>
   ) : (
-    <div className="tasks-container">
-      {tasks.map(({ task_name, date, task_id }) => {
-        return <TaskElement key={task_id} taskName={task_name} date={date} />;
-      })}
-    </div>
+    <>
+      <div className="header-fill">
+        <div className="mobile-header">
+          <img className="logo logo-mobile" alt="logo" src={logo}></img>
+          <div className="logo-text">
+            <Typography variant="h4" lineHeight="1" fontSize="1.5rem">
+              {"יד תמר"}
+            </Typography>
+            <Typography style={{ opacity: "0.5" }} variant="subtitle">
+              {"תמיכה במשפחות חולי סרטן ובמצבי משבר"}
+            </Typography>
+          </div>
+        </div>
+        <div className="buttons-header">
+          <div
+            className="button header-button"
+            onClick={() => {
+              setFilterBy("all");
+            }}
+          >
+            {"רשימה"}
+          </div>
+          <div
+            className="button header-button"
+            onClick={() => {
+              setFilterBy("currentUser");
+            }}
+          >
+            {"המשימות שלי"}
+          </div>
+          <div className="button header-button">{"השבוע"}</div>
+        </div>
+      </div>
+      <div className="tasks-container">
+        {filteredTasks.map(
+          ({ task_name, date, task_id, helper_id, createdAt }) => {
+            return (
+              <TaskElement
+                key={task_id}
+                taskName={task_name}
+                date={date}
+                helper_id={helper_id}
+                setTasks={setTasks}
+                tasks={tasks}
+                createdAt={createdAt}
+              />
+            );
+          }
+        )}
+      </div>
+    </>
   );
 }
 
--- client/src/pages/tasks-page/components/TaskElement.jsx
+++ client/src/pages/tasks-page/components/TaskElement.jsx
@@ -1,8 +1,66 @@
 import moment from "moment";
-import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 
-const TaskElement = ({ taskName, date }) => {
-  console.log(moment(date));
+const TaskElement = ({
+  taskName,
+  date,
+  helper_id,
+  setTasks,
+  tasks,
+  createdAt,
+}) => {
+  const volunteerId = 1;
+
+  const [taskColor, setTaskColor] = useState("");
+  const [takenByUser, setTakenByUser] = useState(false);
+  const [helperText, setHelperText] = useState("");
+
+  useEffect(() => {
+    const taskColorPicker = () => {
+      if (createdAt) {
+        setTaskColor("#e0e0e0");
+      } else if (helper_id === null) {
+        setTaskColor("#9ecf83");
+      } else if (helper_id === volunteerId) {
+        setTaskColor("#8ca8e0");
+      } else if (helper_id) {
+        setTaskColor("#fdf797");
+      }
+    };
+
+    const isTakenPicker = () => {
+      if (createdAt) {
+        setTakenByUser(true);
+      }
+    };
+
+    const helperTextPicker = () => {
+      if (helper_id === null) {
+        setHelperText("פנוי");
+      } else if (helper_id === volunteerId) {
+        setHelperText("אתה");
+      } else if (helper_id) {
+        setHelperText(helper_id);
+      }
+    };
+
+    isTakenPicker();
+    taskColorPicker();
+    helperTextPicker();
+  }, [createdAt, takenByUser, tasks]);
+
+  const handleHelperUpdate = () => {
+    if (helper_id === null) {
+      const taskIndex = tasks.findIndex((task) => {
+        return task.task_name === taskName;
+      });
+      const updatedTasks = [...tasks];
+      updatedTasks[taskIndex].helper_id = volunteerId;
+      setTasks([...updatedTasks]);
+    }
+  };
+
   return (
     <div className="task-element-container">
       <div className="task-header">
@@ -10,13 +68,26 @@ const TaskElement = ({ taskName, date }) => {
         <div className="date">{moment(date).format("MMMM D")}</div>
       </div>
       <div className="element-body">
-        <div className="task">
+        <div
+          className="task"
+          style={{
+            textDecoration: takenByUser ? "line-through" : null,
+            backgroundColor: `${taskColor}`,
+            opacity: "0.8",
+          }}
+        >
           <div className="done-until">{moment(date).format("HH:mm")}</div>
           <div className="task-description">{taskName}</div>
         </div>
-        <div className="done-by">
-          <div className="helper-name">דוד</div>
-          <div className="will-do-it">{"יעשה זאת"}</div>
+        <div
+          className="button"
+          onClick={handleHelperUpdate}
+          style={{ backgroundColor: `${taskColor}` }}
+        >
+          <div className="helper-name">{helperText}</div>
+          <div className="will-do-it">
+            {helper_id ? "יעשה ע''י" : "תוכל לסייע?"}
+          </div>
         </div>
       </div>
     </div>
